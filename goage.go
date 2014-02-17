19a06a9180766ec53a6a8fa3ed5196bec85b8826
Alexander Harkness <bearbin@gmail.com>
2014-02-17 19:51:04 +0000

    Update goage.go

--- goage.go
+++ goage.go
@@ -2,7 +2,7 @@ package goage
 
 import "time"
 
-// Get the age of an entity at a certain time.
+// AgeAt gets the age of an entity at a certain time.
 func AgeAt(birthDate time.Time, now time.Time) int {
 	// Get the year number change since the player's birth.
 	years := now.Year() - birthDate.Year()
@@ -16,7 +16,7 @@ func AgeAt(birthDate time.Time, now time.Time) int {
 	return years
 }
 
-// Age(x) is shorthand for AgeAt(x, time.Now()), and carries the same usage and limitations.
+// Age is shorthand for AgeAt(birthDate, time.Now()), and carries the same usage and limitations.
 func Age(birthDate time.Time) int {
 	return AgeAt(birthDate, time.Now())
 }
@@ -43,7 +43,6 @@ func isLeap(date time.Time) bool {
 		return false
 	} else if year%4 == 0 {
 		return true
-	} else {
-		return false
 	}
+	return false
 }
